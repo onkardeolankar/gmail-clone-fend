aed37ecce58cfc3a4f69c58ffc2e948d10851461
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-12-09 19:05:43 +0530

    auth updated

--- src/Components/Authentication.js
+++ src/Components/Authentication.js
@@ -18,7 +18,7 @@ function Authentication() {
   });
 
   const login = async (userDetails) => {
-  const data=await fetch("https://gmailclon.herokuapp.com/users/login",{
+  const data=await fetch("https://gmail-clone-t228.onrender.com/users/login",{
       method:'POST',
       headers: {
         "Content-type": "application/json",
